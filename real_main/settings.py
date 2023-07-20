c281253e7c47f1518ffb1d01dc65145e12ff8958
Yevgeniy <yevgeniy.tolks@gmail.com>
2023-07-20 18:02:36 +0300

    listing and relator model imprlementation in psql

--- real_main/settings.py
+++ real_main/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'real_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rees_dj_project',
+        'USER':'jevgenijs_tolks',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT':'5432'
     }
 }
 
