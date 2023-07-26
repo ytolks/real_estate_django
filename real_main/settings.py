10324628ef5bda4215153ac5926b281d45998db3
Yevgeniy <yevgeniy.tolks@gmail.com>
2023-07-26 20:37:07 +0300

    remove email conf from settings.py

--- real_main/settings.py
+++ real_main/settings.py
@@ -151,12 +151,6 @@ MESSAGE_TAGS = {
 
 #Email config
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'yevgeniy.tolks@gmail.com'
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
