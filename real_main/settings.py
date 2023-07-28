50ba89b2795894229bb515dac0eba41ebfdbcc46
Yevgeniy <yevgeniy.tolks@gmail.com>
2023-07-29 00:27:02 +0300

    static folder

--- real_main/settings.py
+++ real_main/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT= Path.joinpath(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     Path.joinpath(BASE_DIR, 'real_main/static'),
 ]
